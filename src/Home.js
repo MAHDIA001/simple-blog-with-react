770d6b3129d2a3d6ab703a699065f4ac3de5cffc
MAHDIA001 <mahdianaiebi@gmail.com>
2021-07-31 21:51:10 -0700

    Navbar is done!

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+function Home(){
+   
+        return(
+            <div className="homePage">
+                <h2>
+                    HOME PAGE
+                </h2>
+                
+            </div>
+        )
+   
+}
+export default Home;
---
+++ src/Navbar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+function Navbar(){
+   
+        return(
+            <div>
+                <nav className="navbar">
+             <h1>BLOG</h1>
+             <div className="links">
+             <a href="/">HOME</a>
+             <a href="/create">NEW BLOG</a>
+             </div>
+             </nav>
+
+            </div>
+        )
+   
+}
+export  default Navbar
