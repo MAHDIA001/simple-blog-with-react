56dea9aa2dbb28d7382e8ced2b80653ef8056ca1
MAHDIA001 <mahdianaiebi@gmail.com>
2021-08-01 02:27:48 -0700

    some blogs has been added

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,19 @@
+import React, { useState } from 'react'
+
 const Home=()=>{
-   
+const [blogs,setBlogs] = useState([
+{title: 'My new website!', body:'lorem ipsum...', author:'Mario', id:1},
+{title: 'Welcome party!', body:'lorem ipsum...', author:'Luigi', id:2},
+{title: 'My dev top tips!', body:'lorem ipsum...', author:'Mario', id:3}
+])
         return(
             <div className="home">
-                <h2>
-                    HOME PAGE
-                </h2>
-                
+              {blogs.map((blog)=>(
+<div className="blog-preview" key={blog.id}>
+    <h2>The title of blog: {blog.title}</h2>
+    <p>The author: {blog.author}</p>
+</div>
+              ))}
             </div>
         );
    
