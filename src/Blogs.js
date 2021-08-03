6fe1e3db6b65f1dc1d6d1fbfb4a73d49903d88e4
MAHDIA001 <mahdianaiebi@gmail.com>
2021-08-03 02:16:17 -0700

    error handling

--- src/Blogs.js
+++ src/Blogs.js
@@ -4,17 +4,17 @@ const Blogs = ({blogs,title})=>{
     return(
     <div className="blogs-list">
         <h2>{title}</h2>
-{blogs.map((blog)=>(
-    <div className="blog-preview" key={blog.id}>
+
+    <div className="blog-preview" key={blogs.id}>
         
         
-        <h2>The title of blog: {blog.title}</h2>
-        <p>The author: {blog.author}</p>
+        <h2>The title of blog: {blogs.title}</h2>
+        <p>The author: {blogs.author}</p>
        
 
     </div>
 
-                  ))}
+                  
                   </div>
     )
                 }
--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,35 @@ import Blogs from './Blogs'
 const Home=()=>{
 const [name,setName] = useState('mario')
 const [blogs,setBlogs] = useState(null);
-const deleteBtn =(id)=>{
-const newBlog = blogs.filter(blog=>blog.id!=id)
-setBlogs(newBlog)
-}
-useEffect(() => {
-fetch('http://localhost:8000/blogs')
+const[isPending,setIsPending] = useState(true)
+const [error,setError] =useState(null);
+ useEffect(() => {
+setTimeout(()=>{
+  fetch('http://localhost:8000/blogsa')
 .then(res=>{
+  if(!res.ok){
+  throw Error('could not load the page')
+ 
+  }
   return res.json();
 })
 .then(data=>{
   setBlogs(data);
+  setIsPending(false)
+  setError(null)
+}).catch(err=>{
+  setIsPending(false)
+  setError(err.message)
 })
+},1000)
 }, []);
  return(
       <div className="home">
+        {
+          error && <div>{error}</div>
+        }
+        {isPending && <div>Loading...</div>}
             {blogs && <Blogs blogs={blogs} />}
-            
-
       </div>
         );
    
